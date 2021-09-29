09b2ddc96de3b12285a79e753d0dc74d4bd344e4
MarcPalacio <mpalacio2020@gmail.com>
2021-09-29 02:09:04 -0400

    added exercise24 pseudocode

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution24 {
+
+    public static void main(String[] args){
+        // Print statement asking for two strings
+        // Calls askString that will return a String
+        // Calls function isAnagram that will return true or false
+    }
+
+    public boolean isAnagram(String input1, String input2){
+        // Sets return value to true by default
+        // Creates an arraylist for each input
+        // Remove any spaces since they do not count as letters
+        // Sort the arraylist
+        // if an element in the arraylist at the index doesn't match
+        //      set the return value to false
+        // returns the return value (should be a true or false)
+        return true;
+    }
+
+    public String askString(String question){
+        // Asks and return the user input (string)
+        return "";
+    }
+}
