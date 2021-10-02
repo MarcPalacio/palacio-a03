0a22f1c3844a39546b866945e9a1c86f3e3e2fe5
MarcPalacio <mpalacio2020@gmail.com>
2021-10-02 17:48:34 -0400

    added exercise28 pseudocode

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution28 {
+    public static void main(String[] args){
+        // Asks the user for 5 integers
+        // calls the askNum method
+        // Stores each of the numbers into the corresponding index
+        // Increment the array index by 1
+        // calls the calculateTotal method
+        // Print out the total based off the calculateTotal method
+    }
+
+    private int askNum(String question){
+        // Asks the user for an integer
+        // Use exception handling to make sure only integers are accepted
+        return 0;
+    }
+
+    public int calculateTotal(int[] array){
+        // Takes in the array and loop through it, adding all elements in the array
+        return 0;
+    }
+}
