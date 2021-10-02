55fc15f33708dde573fc78fa266c6e43c8df0a44
MarcPalacio <mpalacio2020@gmail.com>
2021-10-02 16:08:30 -0400

    added exercise27 pseudocode

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,67 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution27 {
+    public static void main(String[] args){
+        // Ask the user for 5 inputs
+        // First name, last name, employee ID, and zipcode
+        // Passes those variables to the validateInput function
+        // Prints out the output from the validateInput function
+    }
+
+    private String askString(String question){
+        // Asks the user for a password (form of string) and returns it
+        return "";
+    }
+
+    private String validateFirstName(String name){
+        // Takes in the first name as a parameter
+        // if length of the name is less than two characters
+        //      add the output statement must be at least 2 characters
+        //      if the length of the name equals zero
+        //          add the output statement that the name must be filled it
+        // return output statement
+        return "";
+    }
+
+    private String validateLastName(String name){
+        // Takes in the last name as a parameter
+        // if length of the name is less than two characters
+        //      add the output statement must be at least 2 characters
+        //      if the length of the name equals zero
+        //          add the output statement that the name must be filled it
+        // return output statement
+        return "";
+    }
+
+    private String validateZipcode(String zipcode){
+        // Takes in the zipcode as a parameter
+        // if the zipcode's length is less than or greater than five
+        //      add the output statement that the zipcode must be a 5 digit numbers
+        // return output statement
+        return "";
+    }
+
+    private String validateID(String ID){
+        // Takes in the ID as a parameter
+        // if the ID doesn't follow the format (2 leters, a hyphen, followed by 4 letters)
+        //      add the output statement that the employee ID must be in a specific format
+        // return output statement
+        return "";
+    }
+
+    public String validateInput(String firstName, String lastName, String zipcode, String ID){
+        // Calls validateFirstName function and adds it to the output string (regardless of adding something or not)
+        // Calls validateLastName function and adds it to the output string (regardless of adding something or not)
+        // Calls validateZipcode function and adds it to the output string (regardless of adding something or not)
+        // Calls validateID function and adds it to the output string (regardless of adding something or not)
+        // Returns the output statement;
+        return "";
+    }
+
+}
