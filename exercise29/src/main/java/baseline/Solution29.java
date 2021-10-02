17b329274056d75a98748f0c8ec46976d4ec9969
MarcPalacio <mpalacio2020@gmail.com>
2021-10-02 19:39:22 -0400

    added exercise29 pseudocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution29 {
+    public static void main(String[] args){
+        // calls the askNum method
+        // calls the calculateYears method
+        // Prints out how many years it will take to double the investment
+    }
+
+    private int askNum(String question){
+        // Asks the user for a number (int)
+        // if the user puts an invalid input
+        //      program will throw an exception and continue to ask the user for input
+        //      until the user enters a valid number
+        return 0;
+    }
+
+    public int calculateYears(int rate){
+        // return 72 divided by the rate to calculate the amount of years
+        return 0;
+    }
+}
