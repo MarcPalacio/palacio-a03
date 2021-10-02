9f03968cd265892e16431d694d645a8e63b08cc8
MarcPalacio <mpalacio2020@gmail.com>
2021-10-02 10:38:13 -0400

    added exercise26 code

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -4,6 +4,7 @@
  */
 
 package baseline;
+import java.util.*;
 
 public class PaymentCalculator {
     private double balance;
@@ -13,6 +14,9 @@ public class PaymentCalculator {
     public PaymentCalculator(double balance, double APR, double monthPay){
         // Constructor of the PaymentCalculator object when called in another class
         // This will set the balance, APR, and monthly payment to be used in another function
+        this.balance = balance;
+        this.APR = APR;
+        this.monthPay = monthPay;
     }
 
     public int calculateMonthsUntilPaidOff(){
@@ -26,6 +30,9 @@ public class PaymentCalculator {
         // -(1/30) will be a constant
         // 1 + b/p * (1 - (1 + i)^30) will be calculated and set to eq1
         // then the output would return -(1/30) * log(eq1) / log(1 + i)
-        return 0;
+        double dailyRate = (APR/100)/365;
+        double eq1 = 1 + (balance/monthPay) * (1 - Math.pow((1 + dailyRate), 30));
+        int numMonths =  (int)Math.ceil((-1 * Math.log(eq1)) / (30*Math.log(1 + dailyRate)));
+        return numMonths;
     }
 }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -13,14 +13,37 @@ public class Solution26 {
         // APR, balance, and how much can be paid per month
         // Creates an instance of the PaymentCalculator class
         // Calls the calculateMonthsUntilPaidOff method from the PaymentCalculator class
+        Solution26 solution = new Solution26();
+        double balance = solution.askNumber("What is your balance? ");
+        double roundedBalance = Math.ceil(balance * 100.0) / 100.0; // Rounds up to the nearest cent
+        double APR = solution.askNumber("What is the APR on the card (as a percent)? ");
+        double monthlyPayment = solution.askNumber("What is the monthly payment you can make? ");
+        double roundedMonthlyPayment = Math.ceil(monthlyPayment * 100.0) / 100.0; // Rounds up to the nearest cent
+
+        PaymentCalculator calculator = new PaymentCalculator(roundedBalance, APR, roundedMonthlyPayment);
+        int numMonths = calculator.calculateMonthsUntilPaidOff();
+        System.out.println("It will take you " + numMonths + " months to pay off this card.");
     }
 
-    public double askNumber(String question){
+    private double askNumber(String question){
         // Asks the user for a number (double)
         // Will use exception handling to catch if the user put in a string
         // Throw error and ask the user to input a number
-        return 0;
+        double output = 0;
+        boolean invalidInput = true;
+        do{
+            invalidInput = false;
+            try{
+                do{
+                    System.out.print(question);
+                    output = in.nextDouble();
+                }while(output < 0);
+            }catch(InputMismatchException e){ // Checks the input to see if it is valid or not
+                invalidInput = true;
+                in.nextLine();
+                System.out.println("Invalid input. Please enter a valid input.");
+            }
+        }while(invalidInput); // Loop while the input is invalid
+        return output;
     }
-
-
 }
