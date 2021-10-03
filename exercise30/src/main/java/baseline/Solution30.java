1ee486cdd59576331ae2ee449e8c88e228ed489a
MarcPalacio <mpalacio2020@gmail.com>
2021-10-02 20:59:45 -0400

    added exercise30 pseudocode

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution30 {
+    public static void main(String[] args){
+        // calls the createMultiplicationTable and prints it out
+    }
+
+    public String createMultiplicationTable(){
+        // have one loop control the row
+        // have one loop control the column
+        // multiply the index of the row and column and add it to the string
+        // if the column equals 12
+        //      add a new line to the output
+        //      *create for if statements for if the input is greater than 9 and 99 in order to indent properly*
+        return "";
+    }
+}
