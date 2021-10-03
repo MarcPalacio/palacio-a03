12d64cf7d5df02e5442c9ad931f900218b3ac6e3
MarcPalacio <mpalacio2020@gmail.com>
2021-10-03 11:18:38 -0400

    added exercise33 code

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -7,21 +7,36 @@ package baseline;
 import java.util.*;
 
 public class Solution33 {
+    private static Scanner in = new Scanner(System.in);
     public static void main(String[] args){
+        Solution33 solution = new Solution33();
         // Stores an arrayList of answers
+        ArrayList<String> listAnswer = new ArrayList<String>();
+        listAnswer.add("Yes.");
+        listAnswer.add("No.");
+        listAnswer.add("Maybe.");
+        listAnswer.add("Ask again later.");
+
         // Calls askQuestion to store question
+        solution.askQuestion("What's your question?\n > ");
         // Calls randomNum to get a number to get an answer from the arrayList of answers
+        int answer = solution.randomNum();
+        String response = listAnswer.get(answer);
         // Prints out answer
+        System.out.println(response);
     }
 
-    private String askQuestion(String question){
+    private void askQuestion(String question){
         // Prompts the user for a question and
         // makes the user input a question
-        return "";
+        System.out.print(question);
+        in.nextLine();
+        System.out.println();
     }
 
     public int randomNum(){
+        Random rand = new Random();
         // returns a random number between 0-3
-        return 0;
+        return rand.nextInt(4);
     }
 }
