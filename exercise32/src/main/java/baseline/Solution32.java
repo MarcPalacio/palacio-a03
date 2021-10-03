69b23671e17b015d7fdbaeece47acfd2cfd68a9a
MarcPalacio <mpalacio2020@gmail.com>
2021-10-03 08:46:43 -0400

    added exercise32 pseudocode

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution32 {
+    public static void main(String[] args){
+        // Prints out the header
+        // while the user wishes to play (with running at least once)
+        //  call verifyNum to select a difficulty
+        //  ensure the difficulty is within [1,3]
+        //  call playGame taking in the difficulty as a parameter
+    }
+
+    public int verifyNum(String question){
+        // asks the user for an int
+        // validates the int
+        return 0;
+    }
+
+    public int playGame(int difficulty){
+        // choose a number based on the difficulty the player has chosen
+        // while the player hasn't guessed the right answer
+        //      call verifyNum to validate the user input (invalid inputs count as wrong guesses)
+        //      if the input does not equal the answer
+        //          if the input is less than answer
+        //              print that the answer is higher
+        //          else if the input is higher than the answer
+        //              print that the answer is lower
+        return 0;
+    }
+
+    public int chooseNumber(int difficulty){
+        // if difficulty is equal to 1
+        //      generate a number between 1-10
+        // else if difficulty is equal to 2
+        //      generate a number between 1-100
+        // else
+        //      generate a number between 1-1000
+        return 0;
+    }
+}
