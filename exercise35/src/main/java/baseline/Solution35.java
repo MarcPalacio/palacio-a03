d47db94d89727cad8b78f64babe1493df173d757
MarcPalacio <mpalacio2020@gmail.com>
2021-10-03 13:15:36 -0400

    added exercise35 pseudocode

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution35 {
+    public static void main(String[] args){
+        // create arraylist of names
+        // calls askName to get a name from the user
+        // if the name does not equal an empty string
+        //      add the name to the arraylist
+        // else
+        //      end the loop
+        // calls get randomNum which passes the list as a parameter
+        // prints out the winner based on the random number generated
+    }
+
+    private String askName(String question){
+        // Asks the user for a name
+        // returns that name
+        return "";
+    }
+
+    public int randomNum(ArrayList<String> list){
+        // Generates a random number based off the size of the list
+        // Returns that number
+        return 0;
+    }
+}
