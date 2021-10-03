3d751a8f4a7c31d1475e2f3f074d3f9a3bf9c1f9
MarcPalacio <mpalacio2020@gmail.com>
2021-10-03 11:38:07 -0400

    added exercise34 pseudocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution34 {
+    public static void main(String[] args){
+        // Create an array list of employees
+        // Print out arraylist of employees
+        // calls askName which stores the string of the name the user wants to remove from list
+        // calls removeName which takes in the list of employees and name of the employee to remove
+        // prints out the new list of employees
+    }
+
+    private String askName(String question){
+        // Asks the user a question (in this case, which employee to remove from the list)
+        // Then returns that name
+        return "";
+    }
+
+    public ArrayList<String> removeName(ArrayList<String> list, String name){
+        ArrayList<String> output = list;
+        // Run through the list of employees
+        // if employee at the current index equals the name of the employee to remove
+        //      remove that name
+        // return the new arrayList
+        return output;
+    }
+}
