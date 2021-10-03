c3de403b0b8da3d7f4b720dd8214809eb21457d6
MarcPalacio <mpalacio2020@gmail.com>
2021-10-03 13:23:27 -0400

    added exercise35 code

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -7,26 +7,44 @@ package baseline;
 import java.util.*;
 
 public class Solution35 {
+    private static Scanner in = new Scanner(System.in);
     public static void main(String[] args){
+        Solution35 solution = new Solution35();
         // create arraylist of names
-        // calls askName to get a name from the user
-        // if the name does not equal an empty string
-        //      add the name to the arraylist
-        // else
-        //      end the loop
+        ArrayList<String> names = new ArrayList<>();
+        String name = "";
+        // while the name does not equal an empty string
+        do{
+            // calls askName to get a name from the user
+            name = solution.askName("Enter a name: ");
+            if(!(name.equals(""))){
+                // if the name does not equal an empty string
+                //      add the name to the arraylist
+                names.add(name);
+            } else {
+                // else
+                //      end the loop
+                break;
+            }
+        }while(!(name.equals("")));
+
         // calls get randomNum which passes the list as a parameter
+        int winner = solution.randomNum(names);
         // prints out the winner based on the random number generated
+        System.out.println("The winner is... " + names.get(winner));
     }
 
     private String askName(String question){
         // Asks the user for a name
+        System.out.print(question);
         // returns that name
-        return "";
+        return in.nextLine();
     }
 
     public int randomNum(ArrayList<String> list){
+        Random rand = new Random();
         // Generates a random number based off the size of the list
         // Returns that number
-        return 0;
+        return rand.nextInt(list.size());
     }
 }
