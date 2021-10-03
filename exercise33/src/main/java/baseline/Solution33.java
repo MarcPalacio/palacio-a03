d9fe0efe23f1a62b05d51ea3dedc3593c767d5a9
MarcPalacio <mpalacio2020@gmail.com>
2021-10-03 11:01:57 -0400

    added exercise33 psuedocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Marc Palacio
+ */
+
+package baseline;
+import java.util.*;
+
+public class Solution33 {
+    public static void main(String[] args){
+        // Stores an arrayList of answers
+        // Calls askQuestion to store question
+        // Calls randomNum to get a number to get an answer from the arrayList of answers
+        // Prints out answer
+    }
+
+    private String askQuestion(String question){
+        // Prompts the user for a question and
+        // makes the user input a question
+        return "";
+    }
+
+    public int randomNum(){
+        // returns a random number between 0-3
+        return 0;
+    }
+}
